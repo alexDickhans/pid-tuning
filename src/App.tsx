14b2711e2baafd04373e282e99e36cc54b61df75
Alexander Dickhans <adickhans@gmail.com>
2025-08-13 22:37:28 -0600

    Fix cloudflare analytics

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,6 @@ export default function App(): JSX.Element {
       <header className="header">
         <div className="container">
           <h1 style={{ margin: 0 }}>Alex Dickhans' PID Simulator</h1>
-          <script defer src='https://static.cloudflareinsights.com/beacon.min.js' data-cf-beacon='{"token": "2efb26592076454f8767229c9cf87341"}'></script>
         </div>
       </header>
       <main className="container">
